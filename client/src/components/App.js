5859f1a25f54ec7412b4ee63d0c818d49d689bed
Andrew Stahl <andrewstahl96@gmail.com>
2022-11-30 18:43:50 -0500

    Added correct path for SignupForm.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import SignUpForm from "../pages/SignUpForm";
+import SignUpForm from "./SignUpForm";
 
 /**
  * App Hierarchy
