5f99479ca68fe747161d9ffd0f917602cc1f0f74
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-01 17:19:32 -0500

    Added full routes for users and login/logout routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # resources :users
+  resources :users, only: [:create, :show, :update, :delete]
   post '/signup', to: 'users#create'
+  post '/me', to: 'users#show'
+  post '/login' to: 'sessions#create'
+  post '/logout' to: 'sessions#delete'
 
   get '/hello', to: 'application#hello_world'
 
