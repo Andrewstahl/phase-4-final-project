bd8ee4700b0a333b0d1bb9dd23ae13f99e8d32d4
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-01 20:22:34 -0500

    Finalized functionality for login/signup components

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,8 @@
+import "../assets/Login.css"
 import React, { useState } from "react";
+import styled from "styled-components";
 import SignUpForm from "../components/SignUpForm";
+import LoginForm from "../components/LoginForm";
 
 function Login({ onLogin }) {
   const [showLogin, setShowLogin] = useState(true)
@@ -8,14 +11,41 @@ function Login({ onLogin }) {
     <>
       {showLogin ? (
         <>
-          <h1>Login Form Placeholder</h1>
+          <LoginForm onLogin={onLogin} />
+          <div className="login__signup__switch__container">  
+            <Divider />
+            <p className="login__signup__switch__header">Don't Have an Account?</p>
+            <button 
+              className="login__signup__switch__button"
+              onClick={() => setShowLogin(false)}
+            >
+              Signup
+            </button>
+          </div>
         </>
       ) : (
         <>
-        
+          <SignUpForm onLogin={onLogin} />
+          <div className="login__signup__switch__container">
+            <Divider />
+            <p className="login__signup__switch__header">Already Have an Account?</p>
+            <button 
+              className="login__signup__switch__button"
+              onClick={() => setShowLogin(true)}
+            >
+              Login
+            </button>
+          </div>
         </>
       )}
     </>
   )
 
 }
+
+const Divider = styled.hr`
+  border-bottom: 1px solid #000;
+  margin: 10px 0;
+`;
+
+export default Login;
