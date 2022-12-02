97698a030037e1251065d23fca4c4c7ceba64dea
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-01 20:43:49 -0500

    Fixed the destroy route for logouts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#delete'
+  delete '/logout', to: 'sessions#destroy'
 
   get '*path',
     to: 'fallback#index',
