2328f8caea97c3617b5d0e8236272af45ccdf717
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-01 20:24:40 -0500

    Removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
 
-  get '/hello', to: 'application#hello_world'
-
   get '*path',
     to: 'fallback#index',
     constraints: ->(req) { !req.xhr? && req.format.html? }
