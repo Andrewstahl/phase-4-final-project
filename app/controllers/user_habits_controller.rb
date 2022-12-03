a66e3484a0f9a6c70afff661f819067a49ec0b54
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-03 11:47:08 -0500

    Added index and show methods

--- app/controllers/user_habits_controller.rb
+++ app/controllers/user_habits_controller.rb
@@ -1,2 +1,16 @@
 class UserHabitsController < ApplicationController
+
+  def index
+    render json: UserHabit.all
+  end
+
+  def show
+    user = User.find(session[:user_id])
+    if user
+      render json: user, include: habits
+    else
+      render json: { error: "Unauthorized User" }, status: unauthorized
+    end
+  end
+
 end
