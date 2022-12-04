8cb9f79648a0d16664b1cdaf682f5f553942d848
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 11:34:08 -0500

    Removed unnecessary code

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password
-  
-  # has_many :user_habits
-  # has_many :habits, through: :user_habits
+
   has_many :habits
 end
