e5e4c911c459eb8df0cede63deda5abf981d67e4
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 07:42:16 -0500

    Added custom routes to edit and delete current user profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :users
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
+  patch '/me', to: 'users#update'
+  delete '/me', to: 'users#destroy'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
