67e48e68add939fc017a940506fc7278a39726a1
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 07:46:45 -0500

    Added routes for updating and deleting current profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
+  skip_before_action :authorized, only: :create
+  
   # GET /users
   def index
     render json: User.all
@@ -8,33 +9,53 @@ class UsersController < ApplicationController
   
   # POST /users
   def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user, status: :created
+    @user = User.create!(user_params)
+    session[:user_id] = @user.id
+    render json: @user, status: :created
   end
   
-  # GET /users/:id
+  # GET /me
   def show
+    @user = User.find(session[:user_id])
+    render json: @user, include: :habits
+  end
+  
+  # PATCH/PUT /me
+  def update
     user = User.find(session[:user_id])
-    if user
-      # render json: user
-      render json: user, include: [
-        :habits,
-        :user_habits
-      ]
-    else
-      render json: { error: "Unauthorized User" }, status: unauthorized
-    end
+    user.update!(user_params)
+    render json: user
+    # if @user
+    #   @user.update!(user_params)
+    #   render json: @user
+    # else
+    #   render_unauthorized_user_response
+    # end
   end
 
+  # DELETE /me
+  def destroy
+    @user = current_user
+    @user.destroy
+    head :no_content
+  end
+    
   private
-
+  
   def user_params
     params.permit(:username, :password, :password_confirmation)
   end
-
+  
+  # def current_user
+  #   @user = User.find(session[:user_id])
+  # end
+  
   def render_unprocessable_entity_response(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+  
+  def render_unauthorized_user_response
+    render json: { error: "Unauthorized User" }, status: unauthorized
   end
 
 end
