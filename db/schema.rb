09dcc6f82f28c4346e814c524a7820943189b753
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 06:25:26 -0500

    Updated schema for log table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_160247) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_192506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "habits", force: :cascade do |t|
     t.string "name"
     t.string "option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "logs", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "habit_id"
+    t.integer "amount"
+    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
