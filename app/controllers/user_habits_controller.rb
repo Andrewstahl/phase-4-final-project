f1c1f546c2237da86ad37793c39859d369ad03ed
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 06:23:34 -0500

    Added create, update, and destroy routes

--- app/controllers/user_habits_controller.rb
+++ app/controllers/user_habits_controller.rb
@@ -1,9 +1,12 @@
 class UserHabitsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  # GET /user_habits
   def index
     render json: UserHabit.all
   end
 
+  # GET /user_habits/:user_id
   def show
     user = User.find(session[:user_id])
     if user
@@ -11,6 +14,42 @@ class UserHabitsController < ApplicationController
     else
       render json: { error: "Unauthorized User" }, status: unauthorized
     end
+  end
+  
+  # POST /user_habits
+  def create
+    user = User.find(session[:user_id])
+    if user
+      user_habit = UserHabit.create!(user_habits_params)
+      render json: user_habit, include: habits
+    end
+  end
+  
+  # PATCH/PUT /user_habits/:id
+  def update
+    user = User.find(session[:user_id])
+    if user
+      user_habit = UserHabit.find(params[:id])
+      user_habit.update(user_habits_params)
+      render json: user_habit
+    end
+  end
+
+  # DELETE /user_habits/:id
+  def destroy
+    user_habit = UserHabit.find(params[:id])
+    user_habit.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_habits_params
+    params.permit(:user_id, :habit_id, :amount, :frequency)
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
