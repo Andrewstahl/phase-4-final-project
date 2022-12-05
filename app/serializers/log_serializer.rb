5496c92711f18f3b37373427e461f0a53dafe802
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-05 16:14:25 -0500

    Altered serializer to include all data points

--- app/serializers/log_serializer.rb
+++ app/serializers/log_serializer.rb
@@ -1,5 +1,5 @@
 class LogSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :habit_id, :amount, :date
-  belongs_to :user
+  attributes :id, :user_id, :habit_id, :amount, :date, :description
+  # belongs_to :user
   belongs_to :habit
 end
