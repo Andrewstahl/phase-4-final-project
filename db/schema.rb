e2d3fd438967eae12db48fba06b4d54a4b39895b
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 19:08:39 -0500

    Removed 'Option' field from Habit and moved it to UserHabit'

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_192506) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_170340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "habits", force: :cascade do |t|
     t.string "name"
-    t.string "option"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -37,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_192506) do
     t.string "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "option"
   end
 
   create_table "users", force: :cascade do |t|
