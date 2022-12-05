24d8ce2ad52218b255048567329934c37981098d
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-05 16:15:16 -0500

    Added relationships and validations

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,8 @@
 class Log < ApplicationRecord
+  belongs_to :user
+  belongs_to :habit
+
+  validates :amount, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
 end
