64fb4160710000a083b064cf6dfdb5972c71750c
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-05 11:48:23 -0500

    Moved Habit into separate component and added handle functions for Adding/Deleting habit

--- client/src/pages/Habits.js
+++ client/src/pages/Habits.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import AddEditHabit from "../components/AddEditHabit";
 // import styled from "styled-components";
+import "../assets/index.css"
+import Habit from "../components/Habit";
 
 function Habits({ user }) {
   const [userHabits, setUserHabits] = useState(user.user_habits)
   const [currentHabit, setCurrentHabit] = useState(undefined)
   const [fetchMethod, setFetchMethod] = useState("POST")
   const [showAddHabit, setShowAddHabit] = useState(false)
+  const [showDelete, setShowDelete] = useState(false)
 
   // useEffect(() => {
   //   fetch("/users")
@@ -15,42 +18,60 @@ function Habits({ user }) {
   // }, [])
 
   const habitElements = userHabits.map(userHabit => {
-    return (
-      <div className="user-habit-element-container">
-        <p key={userHabit.id}>{userHabit.habit.name}</p>
-        <span>{userHabit.option}</span>
-        <button onClick={() => {
-          setCurrentHabit(userHabit.habit)
-          setFetchMethod("PATCH")
-          setShowAddHabit(!showAddHabit)
-        }}></button>
-      </div>
-    )
+    return <Habit key={userHabit.id} userHabit={userHabit} handleClick={() => {
+      setCurrentHabit(userHabit)
+      setFetchMethod("PATCH")
+      setShowAddHabit(!showAddHabit)
+      setShowDelete(true)
+    }}/>
   })
 
-  function handleAddHabit(habit) {
+  function handleAddHabit(newUserHabit) {
     // console.log(habit)
+    setShowAddHabit(false)
+    if (fetchMethod === "POST") {
+      setUserHabits([...userHabits, newUserHabit])
+    } else {
+      const updatedUserHabits = userHabits.map(userHabit => {
+        if (userHabit.id === newUserHabit.id) {
+          return newUserHabit
+        } else {
+          return userHabit
+        }
+      })
+      setUserHabits(updatedUserHabits)
+    }
+  }
+
+  function handleDelete(deletedUserHabit) {
+    setShowAddHabit(false)
+    const updatedUserHabits = userHabits.filter(userHabit => userHabit.id !== deletedUserHabit.id)
+    setUserHabits(updatedUserHabits)
   }
   
   return (
     <>
-      {showAddHabit ?
-        <AddEditHabit 
-          currentHabit={currentHabit} 
-          fetchMethod={fetchMethod} 
-          onSubmit={handleAddHabit} 
-          onCancel={() => setShowAddHabit(!showAddHabit)} 
-        />
-        :
-        null
-      }
       <div className="add-edit-button-div">
         <button className="add-edit-button" onClick={() => {
           setCurrentHabit(undefined)
           setFetchMethod("POST")
           setShowAddHabit(!showAddHabit)
+          setShowDelete(false)
         }}>Add New Habit</button>
       </div>
+      {showAddHabit ?
+        <AddEditHabit 
+          user={user}  
+          currentHabit={currentHabit} 
+          fetchMethod={fetchMethod} 
+          toggleDeleteButton={showDelete}
+          onSubmit={handleAddHabit} 
+          onCancel={() => setShowAddHabit(!showAddHabit)}
+          onDelete={handleDelete}
+        />
+        :
+        null
+      }
       <div className="habit-list-elements-div">
         {habitElements}
       </div>
