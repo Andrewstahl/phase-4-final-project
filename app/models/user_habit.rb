23c8372aacde5676706066a5b1ab129f6aae22ec
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-05 14:57:02 -0500

    Added validater for user_id and habit_id

--- app/models/user_habit.rb
+++ app/models/user_habit.rb
@@ -2,6 +2,8 @@ class UserHabit < ApplicationRecord
   belongs_to :user
   belongs_to :habit
 
+  validates :user_id, uniqueness: { scope: :habit_id, message: "is already chosen for this account. Please add a new habit to this list" }
+
   accepts_nested_attributes_for :habit
 
 end
