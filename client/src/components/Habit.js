0d4ee27e4ed4c4bbc79a027f850193831240ed4a
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-05 11:48:45 -0500

    Separated out Habit into different component

--- client/src/components/Habit.js
+++ client/src/components/Habit.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
-function Habit({ habit }) {
+function Habit({ userHabit, handleClick }) {
 
   return (
     <>
-      
+      <div className="user-habit-element-container">
+        <p key={userHabit.id}>{userHabit.habit.name}</p>
+        <span>
+          {userHabit.amount} x {userHabit.frequency}
+        </span>
+        <button className="user-habit-edit-button" onClick={handleClick}>Edit Habit</button>
+      </div>
     </>
   )
 }
+
+export default Habit;
