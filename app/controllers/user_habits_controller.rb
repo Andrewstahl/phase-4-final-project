2b2594d587d74c3b6c438713faee11414233d3c2
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-04 19:02:06 -0500

    Adjusted show method

--- app/controllers/user_habits_controller.rb
+++ app/controllers/user_habits_controller.rb
@@ -3,14 +3,14 @@ class UserHabitsController < ApplicationController
 
   # GET /user_habits
   def index
-    render json: UserHabit.all
+    render json: UserHabit.all, include: {habit: { only: [:name] }} 
   end
 
   # GET /user_habits/:user_id
   def show
     user = User.find(session[:user_id])
     if user
-      render json: user, include: habits
+      render json: user #, each_serializer: NestedHabitSerializer
     else
       render json: { error: "Unauthorized User" }, status: unauthorized
     end
@@ -20,6 +20,15 @@ class UserHabitsController < ApplicationController
   def create
     user = User.find(session[:user_id])
     if user
+      user_habit = UserHabit.create!({
+        user_id: user.id,
+        habit_id: user_habits_params[:habit_id],
+        option: user_habits_params[:option],
+        amount: user_habits_params[:amount],
+        frequency: user_habits_params[:frequency]
+      })
+      render json: user_habit, include: {habit: { only: [:name] }}
+    else
       user_habit = UserHabit.create!(user_habits_params)
       render json: user_habit, include: habits
     end
@@ -45,7 +54,7 @@ class UserHabitsController < ApplicationController
   private
 
   def user_habits_params
-    params.permit(:user_id, :habit_id, :amount, :frequency)
+    params.permit(:user_id, :habit_id, :option, :amount, :frequency)
   end
 
   def render_unprocessable_entity(invalid)
