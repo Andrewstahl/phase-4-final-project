63c43f2959680eaf5e709f60f43db6aeb2cf2040
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-06 17:30:50 -0500

    Added relationship to habit and created method to pull in the habit name when pulling a log

--- app/serializers/log_serializer.rb
+++ app/serializers/log_serializer.rb
@@ -1,5 +1,10 @@
 class LogSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :habit_id, :amount, :date, :description
+  attributes :id, :user_id, :habit_id, :amount, :date, :description, :own_habit
   # belongs_to :user
   belongs_to :habit
+
+  def own_habit
+    ActiveModel::SerializableResource.new(object.habit)
+  end
+
 end
