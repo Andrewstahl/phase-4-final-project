aad240c51f6c08caa91b91cfc6049d1f73d2e29d
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-07 17:25:25 -0500

    Added log dependency and logic to destroy user_habits and logs if the user or habit is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :user_habits
+  has_many :user_habits, dependent: destroy
   has_many :habits, through: :user_habits
-  has_many :logs
+  has_many :logs, dependent: destroy
 
   has_secure_password
 
