8466de3a0fc93df76021146a620562f9b5042d05
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-06 21:12:42 -0500

    Separated form actions buttons into a separate component

---
+++ client/src/components/FormActionButtons.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FormActionButtons({ currentElement, onDelete, onCancel }) {
+  return (
+    <div className="form-action-buttons">
+      <input type="submit" value="Submit" />
+      {currentElement !== undefined ?
+        <button className="delete" onClick={(e) => onDelete(e)}>Delete</button> :
+        // <button className="delete" onClick={(e) => handleDelete(e)}>Delete</button> :
+        null
+      }
+      <button className="cancel" onClick={onCancel}>Cancel</button>
+    </div>
+  )
+}
