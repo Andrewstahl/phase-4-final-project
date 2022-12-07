5bdfebe9626019da4813669294a599f6e3071285
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-06 22:08:50 -0500

    Syntax clean-up with Prettier, removed unnecessary code

--- client/src/components/Habit.js
+++ client/src/components/Habit.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Habit({ userHabit, handleClick }) {
-
   return (
     <>
       <div className="user-habit-element-container">
-        <h2 className="user-habit-name" key={userHabit.id}>[{userHabit.option}] {userHabit.own_habit.name}</h2>
+        <h2 className="user-habit-name" key={userHabit.id}>
+          [{userHabit.option}] {userHabit.own_habit.name}
+        </h2>
         <p className="user-habit-amount-frequency">
           {userHabit.amount} x {userHabit.frequency}
         </p>
-        <button className="user-habit-edit-button" onClick={handleClick}>Edit Habit</button>
+        <button className="user-habit-edit-button" onClick={handleClick}>
+          Edit Habit
+        </button>
       </div>
     </>
-  )
+  );
 }
 
 export default Habit;
