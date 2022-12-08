d011366be419e26e2d5f06e2159ad9928d0676ce
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-07 20:00:25 -0500

    Added more descriptions to the headers

--- client/src/components/LogEntry.js
+++ client/src/components/LogEntry.js
@@ -1,15 +1,18 @@
 import React from "react";
+import moment from "moment";
 
 export default function LogEntry({ log, handleClick }) {
   return (
     <>
       <div className="log-element-container">
-        <h3 key={log.id} className="log-element-header">
+        <h2 key={log.id} className="log-element-header">
           {log.own_habit.name}
-        </h3>
-        <p className="log-element-date">{log.date.split("T")[0]}</p>
-        <p className="log-element-amount">{log.amount}</p>
-        <p className="log-element-description">{log.description}</p>
+        </h2>
+        <h4 className="log-element-date">{moment(log.date).format("MMM D YYYY, H:mm")}</h4>
+        <p className="log-element-amount">Amount: {log.amount}</p>
+        {log.description !== "" ? (
+          <p className="log-element-description">Note: {log.description}</p>
+        ) : null}
         <button className="log-edit-button" onClick={handleClick}>
           Edit Log Entry
         </button>
