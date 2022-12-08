964e1d443f380fe6bbee2687b650cc15ef2eaceb
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-07 21:20:09 -0500

    Moved add/delete habit functions to App so it can update the habit dropdowns in real-time

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -29,14 +29,40 @@ import Log from "../pages/Log";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [userHabits, setUserHabits] = useState(null);
 
   useEffect(() => {
     fetch("/me").then((r) => {
       if (r.ok) {
-        r.json().then((user) => setUser(user));
+        r.json().then((user) => {
+          setUser(user);
+          setUserHabits(user.user_habits);
+        });
       }
     });
   }, []);
+
+  function handleAddHabit(newUserHabit, fetchMethod) {
+    if (fetchMethod === "POST") {
+      setUserHabits([...userHabits, newUserHabit]);
+    } else {
+      const updatedUserHabits = userHabits.map((userHabit) => {
+        if (userHabit.id === newUserHabit.id) {
+          return newUserHabit;
+        } else {
+          return userHabit;
+        }
+      });
+      setUserHabits(updatedUserHabits);
+    }
+  }
+
+  function handleDelete(deletedUserHabit) {
+    const updatedUserHabits = userHabits.filter(
+      (userHabit) => userHabit.id !== deletedUserHabit.id
+    );
+    setUserHabits(updatedUserHabits);
+  }
 
   if (!user) return <Login onLogin={setUser} />;
 
@@ -46,10 +72,15 @@ function App() {
       <main>
         <Switch>
           <Route exact path="/">
-            <Habits user={user} />
+            <Habits
+              user={user}
+              userHabits={userHabits}
+              onAddHabit={handleAddHabit}
+              onDeleteHabit={handleDelete}
+            />
           </Route>
           <Route exact path="/log">
-            <Log user={user} />
+            <Log user={user} userHabits={userHabits} />
           </Route>
           <Route exact path="/profile">
             <Profile user={user} setUser={setUser} />
