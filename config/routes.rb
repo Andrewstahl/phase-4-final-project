512c5fa90662bb84effb5980bd0248ba94840d91
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-21 10:50:53 -0500

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete '/me', to: 'users#destroy'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
+  
   get '*path',
     to: 'fallback#index',
     constraints: ->(req) { !req.xhr? && req.format.html? }
