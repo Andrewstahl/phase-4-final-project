e7a9053f7f30e703686ad9da8be2f0bfc938ea6b
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-21 10:51:49 -0500

    Added adjustments so that a log or user habit cannot be changed unless you're the user

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,6 +2,8 @@ class LogsController < ApplicationController
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  before_action :get_user, only: [ :create, :update, :destroy ]
+  before_action :get_log, only: [ :show, :update, :destroy ]
 
   # GET /logs
   def index
@@ -10,16 +12,14 @@ class LogsController < ApplicationController
   
   # GET /logs/:id
   def show
-    @log = Log.find(params[:id])
     render json: @log
   end
 
   # POST /logs
   def create
-    user = User.find(session[:user_id])
-    if user
+    if @user
       @log = Log.create!({
-        user_id: user.id,
+        user_id: @user.id,
         habit: Habit.find_by(name: log_params[:habit]),
         amount: log_params[:amount],
         date: log_params[:date],
@@ -34,19 +34,34 @@ class LogsController < ApplicationController
 
   # PATCH/PUT /logs/:id
   def update
-    @log = Log.find(params[:id])
-    @log.update!(log_no_habit_params)
-    render json: @log
+    if @log.user_id == @user.id
+      @log.update!(log_no_habit_params)
+      render json: @log
+    else
+      render_not_authorized_response
+    end
   end
   
   # DELETE /logs/:id
   def destroy
-    @log = Log.find(params[:id])
-    @log.destroy
-    head :no_content
+    if @log.user_id == @user.id
+      @log = Log.find(params[:id])
+      @log.destroy
+      head :no_content
+    else
+      render_not_authorized_response
+    end
   end
 
   private
+
+  def get_user
+    @user = User.find(session[:user_id])
+  end
+
+  def get_log
+    @log = Log.find(params[:id])
+  end
 
   def log_params
     params.permit(:user_id, :habit_id, :amount, :date, :description, :habit)
@@ -62,6 +77,10 @@ class LogsController < ApplicationController
 
   def render_not_found_response
     render json: { errors: ["User log not found"] }, status: :not_found
+  end
+
+  def render_not_authorized_response
+    render json: { error: "Not Authorized" }
   end
 
 end
--- app/controllers/user_habits_controller.rb
+++ app/controllers/user_habits_controller.rb
@@ -1,6 +1,8 @@
 class UserHabitsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  before_action :get_user, only: [ :create, :update, :destroy ]
+  before_action :get_user_habit, only: [ :show, :update, :destroy ]
 
   # GET /user_habits
   def index
@@ -9,43 +11,54 @@ class UserHabitsController < ApplicationController
 
   # GET /user_habits/:user_id
   def show
-    user_habit = UserHabit.find(params[:id])
-    render json: user_habit
+    render json: @user_habit
   end
   
   # POST /user_habits
   def create
-    user = User.find(session[:user_id])
-    if user
-      user_habit = UserHabit.create!({
-        user_id: user.id,
+    if @user
+      @user_habit = UserHabit.create!({
+        user_id: @user.id,
         habit: Habit.find_or_create_by(name: user_habits_params[:habit]),
         option: user_habits_params[:option],
         amount: user_habits_params[:amount],
         frequency: user_habits_params[:frequency]
       })
-      render json: user_habit, status: :created
+      render json: @user_habit, status: :created
     else
-      user_habit = UserHabit.create!(user_habits_params)
-      render json: user_habit, status: :created
+      @user_habit = UserHabit.create!(user_habits_params)
+      render json: @user_habit, status: :created
     end
   end
   
   # PATCH/PUT /user_habits/:id
   def update
-    user_habit = UserHabit.find(params[:id])
-    user_habit.update!(user_habits_no_habit_params)
-    render json: user_habit
+    if @user_habit.user_id == @user.id
+      @user_habit.update!(user_habits_no_habit_params)
+      render json: @user_habit
+    else
+      render_not_authorized_response
   end
 
   # DELETE /user_habits/:id
   def destroy
-    user_habit = UserHabit.find(params[:id])
-    user_habit.destroy
-    head :no_content
+    if @user_habit.user_id == @user.id
+      user_habit.destroy
+      head :no_content
+    else
+      render_not_authorized_response
+    end
   end
 
   private
+
+  def get_user
+    @user = User.find(session[:user_id])
+  end
+  
+  def get_user_habit
+    @user_habit = UserHabit.find(params[:id])
+  end
 
   def user_habits_params
     params.permit(:user_id, :habit_id, :option, :amount, :frequency, :habit)
@@ -61,6 +74,10 @@ class UserHabitsController < ApplicationController
 
   def render_not_found_response
     render json: { errors: ["User habit not found"] }, status: :not_found
+  end
+
+  def render_not_authorized_response
+    render json: { error: "Not Authorized" }
   end
 
 end
